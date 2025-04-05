582dc5b1c8204f5a0cb0df6a1b34e420c368488c
Geraldine743 <egeraldine2@gmail.com>
2025-04-05 11:30:16 +0200

    Add home page

---
+++ templates/footer.php
@@ -0,0 +1,15 @@
+<div class="container">
+        <footer class="py-3 my-4">
+            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Accueil</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Mes listes</a></li>
+            </ul>
+            <p class="text-center text-body-secondary">© 2025 Checkit</p>
+        </footer>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
